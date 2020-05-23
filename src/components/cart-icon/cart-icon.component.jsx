33ce9a79202a365db0753a3c0cf40939e57563d9
Frank Claassen <frank@datajunxion.com>
2020-05-23 10:22:06 +0800

    use contextAPI instead of redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,30 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from "react";
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import "./cart-icon.styles.scss";
+import { CartContext } from "../../providers/cart/cart.provider";
 
-import './cart-icon.styles.scss';
+const CartIcon = () => {
+  const { cartItemsCount, toggleHidden } = useContext(CartContext);
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
-);
+  return (
+    <div className="cart-icon" onClick={toggleHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{cartItemsCount}</span>
+    </div>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-});
-
-const mapStateToProps = createStructuredSelector({
-  itemCount: selectCartItemsCount
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CartIcon);
+export default CartIcon;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCOx1-P07usKOX9H1VcKCbn8w2d4x_QEUQ",
+  authDomain: "crown-db-8af95.firebaseapp.com",
+  databaseURL: "https://crown-db-8af95.firebaseio.com",
+  projectId: "crown-db-8af95",
+  storageBucket: "crown-db-8af95.appspot.com",
+  messagingSenderId: "53815890575",
+  appId: "1:53815890575:web:1574192cbf05e597aa56fa",
+  measurementId: "G-5Q6V1NMSYE",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +30,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
